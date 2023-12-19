f1e720b7a5d65509c3f2bcbf51dc6da0a86b4f84
Carter Nichols <CarterN2500@gmail.com>
2023-12-19 12:38:58 -0500

    Chat input test functionality

---
+++ frontend/src/components/ChatList/index.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+
+export default function ChatList() {
+    // state of conversations
+    const [conversations, setConversations] = useState()
+
+    //
+
+    return (
+        <div className="sidebar">
+            <h1>All chats</h1>
+        </div>
+    )
+}
--- frontend/src/pages/Chat/index.jsx
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect} from 'react'
-
-export default function Chat(props) {
-    return (
-        <h1>Individual chat logs</h1>
-    )
-}
