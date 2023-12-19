d6a3c47d5a40336df37d44afde45922e2ae91486
Ellie Solhjou <elliesolhjou@gmail.com>
2023-12-19 16:39:27 -0600

    API constinously responds

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -1,20 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const chatMessageSchema = new Schema({
+  role: {
+    type: String,
+    enum: ['user', 'assistant', 'system'],
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const chatSchema = new Schema({
-  userInput: {
-    type: String,
-    required: true,
-  },
-  assistantReply: {
-    type: String,
-    required: true,
-  },
+  messages: [chatMessageSchema],
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
 
-module.exports = mongoose.model("Chat", chatSchema);
+
+module.exports = mongoose.model('ChatMessage', chatSchema);
